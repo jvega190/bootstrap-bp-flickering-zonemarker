ff99dd13aded0a3782240838923d0d41bbd510f9
jvega190 <josevega19@gmail.com>
2021-12-10 16:44:05 -0600

    Update button component to handle targets/toggles - js

---
+++ static-assets/js/components/button.js
@@ -0,0 +1,45 @@
+(function() {
+  const buttonsBSProps = {};
+  const buttons = document.querySelectorAll('.ice-btn');
+
+  const getButtonBSProps = (button) => {
+    return button.getAttributeNames().filter((name) => name.startsWith('data-bs'));
+  };
+
+  // Populate buttonsProps
+  buttons.forEach((button) => {
+    const id = button.getAttribute('id');
+    const bsProps = getButtonBSProps(button);
+    const buttonBSProps = {};
+    bsProps.forEach((prop) => {
+      buttonBSProps[prop] = button.getAttribute(prop);
+    });
+    buttonsBSProps[id] = buttonBSProps;
+  });
+
+  const clearButtonsBSProps = () => {
+    buttons.forEach((button) => {
+      const buttonBSProps = getButtonBSProps(button);
+      buttonBSProps.forEach((prop) => {
+        button.removeAttribute(prop);
+      });
+    });
+  }
+
+  const restoreButtonsBSProps = () => {
+    buttons.forEach((button) => {
+      const id = button.getAttribute('id');
+      const buttonBSProps = buttonsBSProps[id];
+      Object.entries(buttonBSProps).forEach(([key, value]) => {
+        button.setAttribute(key, value);
+      });
+    });
+  }
+
+  iceBootstrap.register('button', {
+    iceBypassOn: () => restoreButtonsBSProps(),
+    iceBypassOff: () => clearButtonsBSProps(),
+    onEditModeOn: () => clearButtonsBSProps(),
+    onEditModeOff: () => restoreButtonsBSProps()
+  });
+})();
